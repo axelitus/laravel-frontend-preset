d58f44d3b1159aca46da249642547ee0dc9b10af
Axel Pardemann <axelitusdev@gmail.com>
2018-07-18 21:52:39 -0500

    Modify the page templates
    Adjust the page templates to be more in tune to the Laravel default
    page structure.

--- src/stubs/views/layouts/app.blade.php
+++ src/stubs/views/layouts/app.blade.php
@@ -1,17 +1,24 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-  <head>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
-  </head>
-  <body class="font-sans antialiased text-black leading-tight">
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="font-sans antialiased text-black leading-tight">
     <div id="app">
-      @yield('body')
+        @yield('content')
     </div>
-    <script src="{{ mix('js/app.js') }}"></script>
-  </body>
+</body>
 </html>
--- src/stubs/views/welcome.blade.php
+++ src/stubs/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('body')
+@section('content')
     <div class="min-h-screen flex items-center justify-center">
         <h1 class="text-5xl text-brand font-sans">Ready!</h1>
     </div>
